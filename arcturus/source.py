b001c924a7debebf02ef0cb6fd6c3db8007228b8
wwyaiykycnf <wwyaiykycnf@gmail.com>
2017-10-27 21:17:16 -0700

    initial version of Source / Post classes

---
+++ arcturus/source.py
@@ -0,0 +1,43 @@
+# coding=utf-8
+import datetime
+import abc
+from .blacklist import Filter
+from typing import List
+
+
+class Post:
+    def __init__(self, url, tags, uid, ext):
+        self.url = url
+        self.tags = tags
+        self.id = uid
+        self.ext = ext
+
+    @property
+    def url(self):
+        return self.url
+
+    @property
+    def tags(self):
+        return self.tags
+
+    @property
+    def uid(self):
+        return self.uid
+
+    @property
+    def ext(self):
+        return self.ext
+
+
+class Source(abc.ABC):
+    @abc.abstractmethod
+    def __init__(self,
+                 date: datetime.date = datetime.date.min,
+                 blacklist: Filter = Filter([]),
+                 username=None,
+                 password=None):
+        pass
+
+    @abc.abstractmethod
+    def get_posts(self, query: str, blacklist: filter = Filter) -> List[Post]:
+        pass
