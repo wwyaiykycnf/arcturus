c424ff6d69d9873252cfb0cf1b14087a17c53791
wwyaiykycnf <wwyaiykycnf@gmail.com>
2017-11-12 20:27:27 -0800

    ported from unittest to pytest

--- tests/test_blacklist.py
+++ tests/test_blacklist.py
@@ -1,9 +1,5 @@
 # coding=utf-8
 """tests for blacklist logic"""
-import unittest
-import sys
-
-sys.path.append('../arcturus')
 
 # noinspection PyUnresolvedReferences,PyPep8
 from arcturus.blacklist import Blacklist
@@ -16,108 +12,62 @@ all_posts = [
     ['a', 'b', 'c', 'd', 'e'], ['b', 'c', 'd', 'e', 'f'],
 ]
 
+def test_single_pos_term():
+    f = Blacklist(blacklist=['a'])
+    for post in all_posts:
+        assert ('a' in post) == f.is_blacklisted(post)
 
-class SimpleTests(unittest.TestCase):
-    def test_single_pos_term(self):
-        f = Blacklist(blacklist=['a'])
-        for post in all_posts:
-            if 'a' in post:
-                self.assertTrue(f.is_blacklisted(post))
-            else:
-                self.assertFalse(f.is_blacklisted(post))
+def test_single_negative_term():
+    f = Blacklist(blacklist=['-a'])
+    for post in all_posts:
+        assert ('a' not in post) == f.is_blacklisted(post)
 
-    def test_single_negative_term(self):
-        f = Blacklist(blacklist=['-a'])
-        for post in all_posts:
-            if 'a' not in post:
-                self.assertTrue(f.is_blacklisted(post))
-            else:
-                self.assertFalse(f.is_blacklisted(post))
+def test_two_pos():
+    f = Blacklist(blacklist=['a b'])
+    for post in all_posts:
+        assert ('a' in post and 'b' in post) == f.is_blacklisted(post)
 
+def test_three_pos():
+    f = Blacklist(blacklist=['a b c'])
+    for post in all_posts:
+        assert ('a' in post and 'b' in post and 'c' in post) == f.is_blacklisted(post)
 
-class MultipleItemTests(unittest.TestCase):
-    def test_two_pos(self):
-        f = Blacklist(blacklist=['a b'])
-        for post in all_posts:
-            if 'a' in post and 'b' in post:
-                self.assertTrue(f.is_blacklisted(post))
-            else:
-                self.assertFalse(f.is_blacklisted(post))
+def test_two_negative():
+    f = Blacklist(blacklist=['-a -b'])
+    for post in all_posts:
+        assert ('a' not in post and 'b' not in post) == f.is_blacklisted(post)
 
-    def test_three_pos(self):
-        f = Blacklist(blacklist=['a b c'])
-        for post in all_posts:
-            if 'a' in post and 'b' in post and 'c' in post:
-                self.assertTrue(f.is_blacklisted(post))
-            else:
-                self.assertFalse(f.is_blacklisted(post))
+def test_three_neg():
+    f = Blacklist(blacklist=['-a -b -c'])
+    for post in all_posts:
+        assert ('a' not in post and 'b' not in post and 'c' not in post) == f.is_blacklisted(post)
 
-    def test_two_negative(self):
-        f = Blacklist(blacklist=['-a -b'])
-        for post in all_posts:
-            if 'a' not in post and 'b' not in post:
-                self.assertTrue(f.is_blacklisted(post))
-            else:
-                self.assertFalse(f.is_blacklisted(post))
+def test_pos_neg():
+    f = Blacklist(blacklist=['a -b'])
+    for post in all_posts:
+        assert ('a' in post and 'b' not in post) == f.is_blacklisted(post)
 
-    def test_three_negative(self):
-        f = Blacklist(blacklist=['-a -b -c'])
-        for post in all_posts:
-            if 'a' not in post and 'b' not in post and 'c' not in post:
-                self.assertTrue(f.is_blacklisted(post))
-            else:
-                self.assertFalse(f.is_blacklisted(post))
+def test_pos_two_neg():
+    f = Blacklist(blacklist=['a -b -c'])
+    for post in all_posts:
+        assert('a' in post and 'b' not in post and 'c' not in post) == f.is_blacklisted(post)
 
+def test_two_pos_one_neg():
+    f = Blacklist(blacklist=['a b -c'])
+    for post in all_posts:
+        assert('a' in post and 'b' in post and 'c' not in post) == f.is_blacklisted(post)
 
-class CombinationTests(unittest.TestCase):
-    def test_pos_neg(self):
-        f = Blacklist(blacklist=['a -b'])
-        for post in all_posts:
-            if 'a' in post and 'b' not in post:
-                self.assertTrue(f.is_blacklisted(post))
-            else:
-                self.assertFalse(f.is_blacklisted(post))
+def test_double_pos_double_neg():
+    f = Blacklist(blacklist=['a b -c -d'])
+    for post in all_posts:
+        assert (('a' in post and 'b' in post) and ('c' not in post and 'd' not in post)) == f.is_blacklisted(post)
 
-    def test_pos_two_neg(self):
-        f = Blacklist(blacklist=['a -b -c'])
-        for post in all_posts:
-            if 'a' in post and 'b' not in post and 'c' not in post:
-                self.assertTrue(f.is_blacklisted(post))
-            else:
-                self.assertFalse(f.is_blacklisted(post))
+def test_two_line_simple():
+    f = Blacklist(blacklist=['a', 'b'])
+    for post in all_posts:
+        assert ('a' in post or 'b' in post) == f.is_blacklisted(post)
 
-    def test_two_pos_one_neg(self):
-        f = Blacklist(blacklist=['a b -c'])
-        for post in all_posts:
-            if 'a' in post and 'b' in post and 'c' not in post:
-                self.assertTrue(f.is_blacklisted(post))
-            else:
-                self.assertFalse(f.is_blacklisted(post))
-
-    def test_double_pos_double_neg(self):
-        f = Blacklist(blacklist=['a', 'b', '-c', '-d'])
-        for post in all_posts:
-            if ('a' in post and 'b' in post) and ('c' not in post and 'd' not in post):
-                self.assertTrue(f.is_blacklisted(post))
-
-
-class MultiLineBlacklist(unittest.TestCase):
-    def test_two_line_simple(self):
-        f = Blacklist(blacklist=['a', 'b'])
-        for post in all_posts:
-            if 'a' in post or 'b' in post:
-                self.assertTrue(f.is_blacklisted(post))
-            else:
-                self.assertFalse(f.is_blacklisted(post))
-
-    def test_two_line_neg(self):
-        f = Blacklist(blacklist=['-a', '-b'])
-        for post in all_posts:
-            if 'a' not in post or 'b' not in post:
-                self.assertTrue(f.is_blacklisted(post))
-            else:
-                self.assertFalse(f.is_blacklisted(post))
-
-
-if __name__ == '__main__':
-    unittest.main(verbosity=2)
+def test_two_line_neg():
+    f = Blacklist(blacklist=['-a', '-b'])
+    for post in all_posts:
+        assert ('a' not in post or 'b' not in post) == f.is_blacklisted(post)
